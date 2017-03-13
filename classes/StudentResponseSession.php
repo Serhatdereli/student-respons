179507a2cd5ad923faa3e537aa7d006830a53e0c
Serhat Dereli <sdereli1993@gmail.com>
2017-03-13 22:05:38 +0000

    Creating and getting Session objects

--- classes/StudentResponseSession.php
+++ classes/StudentResponseSession.php
@@ -3,36 +3,63 @@
 class StudentResponseSession
 {
 	private $id;
+	private $user_id;
 	private $created_at;
 	private $expires_at;
 	private $description;
 	private $responses = array();
+
+	public function isExpired()
+	{
+		$now = time();
+		$expires_ts = strtotime($this->expires_at);
+		return ($now > $expires_ts);
+	}
+
+	public static function buildByRow($row)
+	{
+		$session = new self();
+		$session->id = $row['session_id'];
+		$session->user_id = $row['user_id'];
+		$session->created_at = $row['created_at'];
+		$session->expires_at = $row['expires_at'];
+		$session->description = $row['description'];
+		return $session;
+	}
 
 	public static function getByID($session_id)
 	{
 		$db = Database::get();
 		$sql = "SELECT * FROM sr_session WHERE session_id=?";
 		$st = $db->prepare($sql);
-		$st->execute(array($session_id));
-		$res = $st->fetch(PDO::FETCH_ASSOC);
-		// Build the object
-		// Return the object
+		if ($st->execute(array($session_id)))
+		{
+			if ($st->rowCount() > 0)
+			{
+				$row = $st->fetch(PDO::FETCH_ASSOC);
+				$session = self::buildByRow($row);
+				return $session;
+			}
+		}
+		return null;
 	}
 
 	public static function createNew(User $user, $expires_at, $description)
 	{
 		$db = Database::get();
-		$sql = "INSERT INTO sr_session (user_id, created_at, expires_at, description) VALUES (NOW(), ?, ?)";
+		$sql = "INSERT INTO sr_session (user_id, created_at, expires_at, description) VALUES (?, NOW(), ?, ?)";
 		$st = $db->prepare($sql);
-		$st->execute(array($user->getID(), $expires_at, $description));
+		return $st->execute(array($user->getID(), $expires_at, $description));
 	}
 
 	public static function isValidSessionID($session_id)
 	{
-		// check if exists
-		// check if not expired
-		// returns true or false
-		return false;
+		$session = self::getByID($session_id);
+		if (is_null($session))
+		{
+			return false;
+		}
+		return !$session->isExpired();
 	}
 
 	public function getResponses()
--- public/api/get.php
+++ public/api/get.php
@@ -2,24 +2,32 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../app/start.php';
 
-$email = 'serhat@student-response.club';
-$password = 'serhatsux1234';
+// $email = 'serhat@student-response.club';
+// $password = 'serhatsux1234';
 
-try
-{
-	User::register($email, $password);
-}
-catch (Exception $ex) {}
+// try
+// {
+// 	User::register($email, $password);
+// }
+// catch (Exception $ex) {}
 
-$try_pass = 'serhatsux1234';
+// $try_pass = 'serhatsux1234';
 
-if (User::login($email, $try_pass))
-{
-	echo 'LOGGEDIN!';
-}
-else
-{
-	echo 'FAILED TO LOGIN';
-}
+// if (User::login($email, $try_pass))
+// {
+// 	echo 'LOGGEDIN!';
+// }
+// else
+// {
+// 	echo 'FAILED TO LOGIN';
+// }
+
+$user = User::getByEmail('serhat@student-response.club');
+
+// date("Y-m-d H:i:s")
+$expires_at = '2017-03-13 23:00:00';
+$description = 'This is a test session description';
+
+// StudentResponseSession::createNew($user, $expires_at, $description);
 
 exit;
